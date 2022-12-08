c6f811ae4448f9a89cce6e9d8352b8e465c5dc0f
fsmosca <fsmosca@users.noreply.github.com>
2022-12-08 13:04:54 +0800

    Create login.py

---
+++ modules/login.py
@@ -0,0 +1,34 @@
+"""login module
+
+Manages user authentication. It supports sports main page.
+"""
+
+
+import streamlit as st
+from streamlit_login_auth_ui.widgets import __login__
+
+
+class Login:
+    def __init__(self):
+        pass
+
+    def login_auth(self, auth_token='courier_auth_token'):
+        ret = __login__(
+            auth_token=auth_token,
+            company_name="My company name",
+            width=200,
+            height=250,
+            logout_button_name='Logout',
+            hide_menu_bool=False,
+            hide_footer_bool=False,
+            lottie_url='https://assets2.lottiefiles.com/packages/lf20_jcikwtux.json'
+        )
+        return ret
+
+
+def get_username(logauth):
+    if not st.session_state['LOGOUT_BUTTON_HIT']:
+        fetched_cookies = logauth.cookies
+        if '__streamlit_login_signup_ui_username__' in fetched_cookies.keys():
+            username = fetched_cookies['__streamlit_login_signup_ui_username__']
+            return username
